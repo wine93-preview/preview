21f08b5f34611110a40e61fb757998ddbb424791
meilirensheng2020 <yansp@zetyun.com>
2024-08-29 12:18:12 +0800

    [Fix] Unit test compilation issue

--- curvefs/src/client/fuse_volume_client.h
+++ curvefs/src/client/fuse_volume_client.h
@@ -14,7 +14,6 @@
  *  limitations under the License.
  */
 
-
 /*
  * Project: curve
  * Created Date: Thur May 27 2021
@@ -41,81 +40,70 @@ using ::curvefs::volume::SpaceManager;
 
 class FuseVolumeClient : public FuseClient {
  public:
-    FuseVolumeClient()
-        : FuseClient(),
-          blockDeviceClient_(std::make_shared<BlockDeviceClientImpl>()) {}
+  FuseVolumeClient()
+      : FuseClient(),
+        blockDeviceClient_(std::make_shared<BlockDeviceClientImpl>()) {}
 
-    // for UNIT_TEST
-    FuseVolumeClient(
-        const std::shared_ptr<MdsClient> &mdsClient,
-        const std::shared_ptr<MetaServerClient> &metaClient,
-        const std::shared_ptr<InodeCacheManager> &inodeManager,
-        const std::shared_ptr<DentryCacheManager> &dentryManager,
-        const std::shared_ptr<BlockDeviceClient> &blockDeviceClient)
-        : FuseClient(mdsClient, metaClient, inodeManager, dentryManager,
-                     nullptr),
-          blockDeviceClient_(blockDeviceClient) {}
+  // for UNIT_TEST
+  FuseVolumeClient(const std::shared_ptr<MdsClient>& mdsClient,
+                   const std::shared_ptr<MetaServerClient>& metaClient,
+                   const std::shared_ptr<InodeCacheManager>& inodeManager,
+                   const std::shared_ptr<DentryCacheManager>& dentryManager,
+                   const std::shared_ptr<BlockDeviceClient>& blockDeviceClient)
+      : FuseClient(mdsClient, metaClient, inodeManager, dentryManager, nullptr),
+        blockDeviceClient_(blockDeviceClient) {}
 
-    CURVEFS_ERROR Init(const FuseClientOption &option) override;
+  CURVEFS_ERROR Init(const FuseClientOption& option) override;
 
-    void UnInit() override;
+  void UnInit() override;
 
-    CURVEFS_ERROR FuseOpInit(
-        void *userdata, struct fuse_conn_info *conn) override;
+  CURVEFS_ERROR FuseOpInit(void* userdata,
+                           struct fuse_conn_info* conn) override;
 
-    CURVEFS_ERROR FuseOpWrite(fuse_req_t req, fuse_ino_t ino,
-        const char *buf, size_t size, off_t off,
-        struct fuse_file_info *fi, FileOut* fileOut) override;
+  CURVEFS_ERROR FuseOpWrite(fuse_req_t req, fuse_ino_t ino, const char* buf,
+                            size_t size, off_t off, struct fuse_file_info* fi,
+                            FileOut* fileOut) override;
 
-    CURVEFS_ERROR FuseOpRead(fuse_req_t req,
-            fuse_ino_t ino, size_t size, off_t off,
-            struct fuse_file_info *fi,
-            char *buffer,
-            size_t *rSize) override;
+  CURVEFS_ERROR FuseOpRead(fuse_req_t req, fuse_ino_t ino, size_t size,
+                           off_t off, struct fuse_file_info* fi, char* buffer,
+                           size_t* rSize) override;
 
-    CURVEFS_ERROR FuseOpCreate(fuse_req_t req,
-                               fuse_ino_t parent,
-                               const char* name,
-                               mode_t mode,
-                               struct fuse_file_info* fi,
-                               EntryOut* entryOut) override;
-
-    CURVEFS_ERROR FuseOpMkNod(fuse_req_t req,
-                              fuse_ino_t parent,
-                              const char* name,
-                              mode_t mode,
-                              dev_t rdev,
-                              EntryOut* entryOut) override;
-
-    CURVEFS_ERROR FuseOpLink(fuse_req_t req, fuse_ino_t ino,
-                             fuse_ino_t newparent,
-                             const char *newname,
+  CURVEFS_ERROR FuseOpCreate(fuse_req_t req, fuse_ino_t parent,
+                             const char* name, mode_t mode,
+                             struct fuse_file_info* fi,
                              EntryOut* entryOut) override;
 
-    CURVEFS_ERROR FuseOpUnlink(fuse_req_t req, fuse_ino_t parent,
-                               const char *name) override;
+  CURVEFS_ERROR FuseOpMkNod(fuse_req_t req, fuse_ino_t parent, const char* name,
+                            mode_t mode, dev_t rdev,
+                            EntryOut* entryOut) override;
 
-    CURVEFS_ERROR FuseOpFsync(fuse_req_t req, fuse_ino_t ino, int datasync,
-                              struct fuse_file_info *fi) override;
+  CURVEFS_ERROR FuseOpLink(fuse_req_t req, fuse_ino_t ino, fuse_ino_t newparent,
+                           const char* newname, EntryOut* entryOut) override;
 
-    CURVEFS_ERROR FuseOpFlush(fuse_req_t req, fuse_ino_t ino,
-                              struct fuse_file_info *fi) override;
+  CURVEFS_ERROR FuseOpUnlink(fuse_req_t req, fuse_ino_t parent,
+                             const char* name) override;
 
-    CURVEFS_ERROR Truncate(InodeWrapper *inode, uint64_t length) override;
+  CURVEFS_ERROR FuseOpFsync(fuse_req_t req, fuse_ino_t ino, int datasync,
+                            struct fuse_file_info* fi) override;
 
-    void SetSpaceManagerForTesting(SpaceManager *manager);
+  CURVEFS_ERROR FuseOpFlush(fuse_req_t req, fuse_ino_t ino,
+                            struct fuse_file_info* fi) override;
 
-    void SetVolumeStorageForTesting(VolumeStorage *storage);
+  CURVEFS_ERROR Truncate(InodeWrapper* inode, uint64_t length) override;
+
+  void SetSpaceManagerForTesting(SpaceManager* manager);
+
+  void SetVolumeStorageForTesting(VolumeStorage* storage);
 
  private:
-    void FlushData() override;
+  void FlushData() override;
 
  private:
-    std::shared_ptr<BlockDeviceClient> blockDeviceClient_;
-    std::unique_ptr<SpaceManager> spaceManager_;
-    std::unique_ptr<VolumeStorage> storage_;
+  std::shared_ptr<BlockDeviceClient> blockDeviceClient_;
+  std::unique_ptr<SpaceManager> spaceManager_;
+  std::unique_ptr<VolumeStorage> storage_;
 
-    VolumeOption volOpts_;
+  VolumeOption volOpts_;
 };
 
 }  // namespace client
